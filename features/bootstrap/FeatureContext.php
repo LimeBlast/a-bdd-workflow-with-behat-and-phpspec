00b5226e2f81d041414b79a4ae83cb4badcca215
Daniel Hollands <daniel@limeblast.co.uk>
2014-07-13 10:26:31 +0100

    Added and generated initial features

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,5 +1,6 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
@@ -17,5 +18,61 @@ class FeatureContext implements SnippetAcceptingContext
      */
     public function __construct()
     {
+    }
+
+    /**
+     * @Given there is a configuration file
+     */
+    public function thereIsAConfigurationFile()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given the option :arg1 is configured to :arg2
+     */
+    public function theOptionIsConfiguredTo($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I load the configuration file
+     */
+    public function iLoadTheConfigurationFile()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should get :arg1 as :arg2 option
+     */
+    public function iShouldGetAsOption($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given the option :arg1 is not yet configured
+     */
+    public function theOptionIsNotYetConfigured($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should get default value :arg1 as :arg2 option
+     */
+    public function iShouldGetDefaultValueAsOption($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I set the :arg1 configuration option to :arg2
+     */
+    public function iSetTheConfigurationOptionTo($arg1, $arg2)
+    {
+        throw new PendingException();
     }
 }
