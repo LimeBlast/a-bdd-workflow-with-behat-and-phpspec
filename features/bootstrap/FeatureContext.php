15eaaa088518788c694c817015b6ed3638e8dd5a
Daniel Hollands <daniel@limeblast.co.uk>
2014-07-13 10:44:12 +0100

    finished

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,78 +1,105 @@
 <?php
 
-use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\SnippetAcceptingContext;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
 
 /**
  * Behat context class.
  */
-class FeatureContext implements SnippetAcceptingContext
-{
-    /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context object.
-     * You can also pass arbitrary arguments to the context constructor through behat.yml.
-     */
-    public function __construct()
-    {
-    }
+class FeatureContext implements SnippetAcceptingContext {
 
-    /**
-     * @Given there is a configuration file
-     */
-    public function thereIsAConfigurationFile()
-    {
-        throw new PendingException();
-    }
+	/**
+	 * Initializes context.
+	 *
+	 * Every scenario gets its own context object.
+	 * You can also pass arbitrary arguments to the context constructor through behat.yml.
+	 */
+	public function __construct()
+	{
+	}
 
-    /**
-     * @Given the option :arg1 is configured to :arg2
-     */
-    public function theOptionIsConfiguredTo($arg1, $arg2)
-    {
-        throw new PendingException();
-    }
+	/**
+	 * @Given there is a configuration file
+	 */
+	public function thereIsAConfigurationFile()
+	{
+		if (! file_exists('fixtures/config.php'))
+			throw new Exception("File 'fixtures/config.php' not found");
 
-    /**
-     * @When I load the configuration file
-     */
-    public function iLoadTheConfigurationFile()
-    {
-        throw new PendingException();
-    }
+		$config = include 'fixtures/config.php';
 
-    /**
-     * @Then I should get :arg1 as :arg2 option
-     */
-    public function iShouldGetAsOption($arg1, $arg2)
-    {
-        throw new PendingException();
-    }
+		if (! is_array($config))
+			throw new Exception("File 'fixtures/config.php' should contain an array");
 
-    /**
-     * @Given the option :arg1 is not yet configured
-     */
-    public function theOptionIsNotYetConfigured($arg1)
-    {
-        throw new PendingException();
-    }
+	}
 
-    /**
-     * @Then I should get default value :arg1 as :arg2 option
-     */
-    public function iShouldGetDefaultValueAsOption($arg1, $arg2)
-    {
-        throw new PendingException();
-    }
+	/**
+	 * @Given the option :option is configured to :value
+	 */
+	public function theOptionIsConfiguredTo($option, $value)
+	{
+		$config = include 'fixtures/config.php';
 
-    /**
-     * @When I set the :arg1 configuration option to :arg2
-     */
-    public function iSetTheConfigurationOptionTo($arg1, $arg2)
-    {
-        throw new PendingException();
-    }
+		if (! is_array($config)) $config = [];
+
+		$config[$option] = $value;
+
+		$content = "<?php\n\nreturn " . var_export($config, true) . ";\n";
+
+		file_put_contents('fixtures/config.php', $content);
+	}
+
+	/**
+	 * @When I load the configuration file
+	 */
+	public function iLoadTheConfigurationFile()
+	{
+		$this->config = Config::load('fixtures/config.php'); // Taylor!
+	}
+
+	/**
+	 * @Then I should get :value as :option option
+	 */
+	public function iShouldGetAsOption($value, $option)
+	{
+		$actual = $this->config->get($option); // Taylor!
+
+		if (! strcmp($value, $actual) == 0)
+			throw new Exception("Expected {$actual} to be '{$option}'.");
+	}
+
+	/**
+	 * @Given the option :option is not yet configured
+	 */
+	public function theOptionIsNotYetConfigured($option)
+	{
+		$config = include 'fixtures/config.php';
+
+		if (! is_array($config)) $config = [];
+
+		unset($config[$option]);
+
+		$content = "<?php\n\nreturn " . var_export($config, true) . ";\n";
+
+		file_put_contents('fixtures/config.php', $content);
+	}
+
+	/**
+	 * @Then I should get default value :default as :option option
+	 */
+	public function iShouldGetDefaultValueAsOption($default, $option)
+	{
+		$actual = $this->config->get($option, $default); // Taylor!
+
+		if (! strcmp($default, $actual) == 0)
+			throw new Exception("Expected {$actual} to be '{$default}'.");
+	}
+
+	/**
+	 * @When I set the :option configuration option to :value
+	 */
+	public function iSetTheConfigurationOptionTo($option, $value)
+	{
+		$this->config->set($option, $value); // Taylor!
+	}
+
 }
